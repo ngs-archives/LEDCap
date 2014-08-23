1fca3dec5c21f26b77baf88062250064dfe270ca
Atsushi Nagase <a@ngs.io>
2014-08-23 21:25:01 +0900

    Fix bug

--- LEDCap.ino
+++ LEDCap.ino
@@ -1,8 +1,9 @@
 const int temt6000Pin = A5;
-const int ledPins[] = {5, 6, 9, 10, 11, A2, A3, A4};
-const int ledSize = sizeof(ledPins);
-const int threshold = 40;
-const int ledDelay = 100;
+const int ledPins[] = {
+  5, 6, 9, 10, 11, A2, A3, A4};
+const int ledSize = 8;
+const int threshold = 20;
+const int ledDelay = 500;
 int ledCursor;
 
 void setup() {
@@ -21,18 +22,30 @@ boolean isDark() {
 void allOff() {
   for(int i = 0; i < ledSize; i++) {
     digitalWrite(ledPins[i], LOW);
+    analogWrite(ledPins[i], 0);
+  }
+}
+
+void allOn() {
+  for(int i = 0; i < ledSize; i++) {
+    digitalWrite(ledPins[i], HIGH);
+    analogWrite(ledPins[i], 0xff);
   }
 }
 
 void loop() {
   if(isDark()) {
     digitalWrite(ledPins[ledCursor], HIGH);
+    analogWrite(ledPins[ledCursor], 0xff);
     delay(ledDelay);
-  } else {
+    digitalWrite(ledPins[ledCursor], LOW);
+    analogWrite(ledPins[ledCursor], 0);
+    if(++ledCursor == ledSize) {
+      ledCursor = 0;
+    }
+  }
+  else {
     allOff();
   }
-  digitalWrite(ledPins[ledCursor], LOW);
-  if(ledCursor++ == ledSize) {
-    ledCursor = 0;
-  }
 }
+
